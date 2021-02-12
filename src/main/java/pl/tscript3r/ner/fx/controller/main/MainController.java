473d7ab685dbfc0f8e29da69550bc38bae80d9bc
tscript3r <alek199202@gmail.com>
2021-02-12 19:50:27 +0100

    Refactor MainController

--- src/main/java/pl/tscript3r/ner/fx/controller/main/MainController.java
+++ src/main/java/pl/tscript3r/ner/fx/controller/main/MainController.java
@@ -1,51 +1,41 @@
 package pl.tscript3r.ner.fx.controller.main;
 
-import javafx.application.HostServices;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Component;
 import pl.tscript3r.ner.fx.util.SearchFacade;
 import pl.tscript3r.ner.fx.util.SearchTextField;
-import pl.tscript3r.ner.migrate.MigrateFacade;
 import pl.tscript3r.ner.order.OrderEntity;
 
-import java.sql.SQLException;
 import java.time.LocalDate;
 
 @Component
 public class MainController implements DisposableBean {
 
-    private final HostServices hostServices;
-    private final MigrateFacade migrateFacade;
     private final SearchFacade searchFacade;
-    @FXML
-    public TextField textField;
-
-    @FXML
-    public GridPane gridPane;
-    @FXML
-    public TableView<OrderEntity> tableView;
-    @FXML
-    public ProgressIndicator progressIndicator;
     private SearchTextField searchField;
 
-    public MainController(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") HostServices hostServices,
-                          MigrateFacade migrateFacade, SearchFacade searchFacade) {
-        this.hostServices = hostServices;
-        this.migrateFacade = migrateFacade;
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private TableView<OrderEntity> tableView;
+
+    @FXML
+    private ProgressIndicator progressIndicator;
+
+    public MainController(SearchFacade searchFacade) {
         this.searchFacade = searchFacade;
     }
 
     @FXML
-    public void initialize() throws SQLException {
+    public void initialize() {
         searchField = new SearchTextField(textField, this::updateRows);
-        gridPane.setStyle("-fx-background-color: #ffffff;");
         addColumns();
         addInitialRows();
     }
@@ -56,24 +46,24 @@ public class MainController implements DisposableBean {
     }
 
     private TableColumn<OrderEntity, String> getClientColumns() {
-        var customer = getColumn("Kunde");
-        var company = getColumn("Firma");
+        TableColumn<OrderEntity, String> customer = getColumn("Kunde");
+        TableColumn<OrderEntity, String> company = getColumn("Firma");
         company.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(cellData.getValue().getClient().getCompany())
         );
-        var contact = getColumn("Kontaktperson");
+        TableColumn<OrderEntity, String> contact = getColumn("Kontaktperson");
         contact.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(cellData.getValue().getClient().getContactName())
         );
-        var street = getColumn("Strasse");
+        TableColumn<OrderEntity, String> street = getColumn("Strasse");
         street.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(cellData.getValue().getClient().getStreet())
         );
-        var city = getColumn("Ort");
+        TableColumn<OrderEntity, String> city = getColumn("Ort");
         city.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(cellData.getValue().getClient().getCity())
         );
-        var postcode = getColumn("Postleitzahl");
+        TableColumn<OrderEntity, String> postcode = getColumn("Postleitzahl");
         postcode.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(cellData.getValue().getClient().getPostcode())
         );
@@ -86,16 +76,16 @@ public class MainController implements DisposableBean {
     }
 
     private TableColumn<OrderEntity, String> getOrderColumns() {
-        var order = getColumn("Auftrag");
-        var dateOfReceipt = getColumn("Datum des Erhalts");
+        TableColumn<OrderEntity, String> order = getColumn("Auftrag");
+        TableColumn<OrderEntity, String> dateOfReceipt = getColumn("Datum des Erhalts");
         dateOfReceipt.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(date2String(cellData.getValue().getDateOfReceipt()))
         );
-        var description = getColumn("Beschreibung");
+        TableColumn<OrderEntity, String> description = getColumn("Beschreibung");
         description.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(cellData.getValue().getDescription())
         );
-        var dateOfCompletion = getColumn("Datum der Erledigung");
+        TableColumn<OrderEntity, String> dateOfCompletion = getColumn("Datum der Erledigung");
         dateOfCompletion.setCellValueFactory(cellData ->
                 new SimpleObjectProperty<>(date2String(cellData.getValue().getDateOfCompletion()))
         );
@@ -119,7 +109,7 @@ public class MainController implements DisposableBean {
     }
 
     @Override
-    public void destroy() throws Exception {
+    public void destroy() {
         searchField.destroy();
     }
 
